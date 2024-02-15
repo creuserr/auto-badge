ad4980ab991f900117a1cb362ca7e0baf9eb338b
creuser <151720755+creuserr@users.noreply.github.com>
2024-02-15 22:55:56 +0800

    update index.py

--- api/index.py
+++ api/index.py
@@ -8,7 +8,10 @@ class handler(BaseHTTPRequestHandler):
     req = requests.get('https://creprox.vercel.app/https:/api.github.com/repos/' + '/'.join(path[1:]))
     req = json.loads(req.text)
     url = 'https://img.shields.io/badge/'
-    
+    if path[0] == 'stars':
+      url += f'stars-{req["stargazers_count"]}-yellow'
+    elif path[0] == 'watchers':
+      url += f'stars-{req["starga_count"]}-yellow'
     self.send_response(302)
     self.send_header('Location', '')
     self.end_headers()
