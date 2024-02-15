542686de0f65cf453cef798076afbc8a7da21f0d
creuser <151720755+creuserr@users.noreply.github.com>
2024-02-15 22:53:49 +0800

    update index.py

--- api/index.py
+++ api/index.py
@@ -4,9 +4,10 @@ import json
 
 class handler(BaseHTTPRequestHandler):
   def do_GET(self):
-    req = requests.get('https://creprox.vercel.app/https:/api.github.com/repos/' + self.path[1:])
+    path = self.path.split('/')
+    req = requests.get('https://creprox.vercel.app/https:/api.github.com/repos/' + path[1:])
     req = json.loads(req.text)
-    self.send_response(200)
-    self.send_header('Content-type', 'text/plain')
+    self.send_response(302)
+    self.send_header('Location', '')
     self.end_headers()
     self.wfile.write(req.encode('utf-8'))
