e011b0aba775e592d579f0fd5b127f63f9e686ff
creuser <151720755+creuserr@users.noreply.github.com>
2024-02-15 23:02:26 +0800

    update index.py

--- api/index.py
+++ api/index.py
@@ -4,18 +4,21 @@ import json
 
 class handler(BaseHTTPRequestHandler):
   def do_GET(self):
-    path = self.path.split('/')
-    req = requests.get('https://creprox.vercel.app/https:/api.github.com/repos/' + '/'.join(path[1:]))
-    req = json.loads(req.text)
     url = 'https://img.shields.io/badge/'
-    if path[0] == 'star':
+    path = self.path.split('/')
+    try: 
+      req = requests.get('https://creprox.vercel.app/https:/api.github.com/repos/' + '/'.join(path[1:]))
+      req = json.loads(req.text)
+      if path[0] == 'star':
       url += f'stargazers-{req["stargazers_count"]}-yellow'
-    elif path[0] == 'watch':
-      url += f'watchers-{req["watchers_count"]}-violet'
-    elif path[0] == 'sub':
-      url += f'subscribers-{req["subscribers_count"]}-peach'
-    else:
-      url += f'400 Bad Request-Invalid method-lightred'
+      elif path[0] == 'watch':
+        url += f'watchers-{req["watchers_count"]}-violet'
+      elif path[0] == 'sub':
+        url += f'subscribers-{req["subscribers_count"]}-peach'
+      else:
+        url += f'400 Bad Request-Invalid method-lightred'
+    except:
+      url += f'400 Bad Request-lightred'
     self.send_response(302)
     self.send_header('Location', url)
     self.end_headers()
