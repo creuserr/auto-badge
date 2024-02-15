27d128d2afa0a063c10e7f94a263a883d3f764ea
creuser <151720755+creuserr@users.noreply.github.com>
2024-02-15 22:54:52 +0800

    update index.py

--- api/index.py
+++ api/index.py
@@ -5,8 +5,10 @@ import json
 class handler(BaseHTTPRequestHandler):
   def do_GET(self):
     path = self.path.split('/')
-    req = requests.get('https://creprox.vercel.app/https:/api.github.com/repos/' + path[1:])
+    req = requests.get('https://creprox.vercel.app/https:/api.github.com/repos/' + '/'.join(path[1:]))
     req = json.loads(req.text)
+    url = 'https://img.shields.io/badge/'
+    
     self.send_response(302)
     self.send_header('Location', '')
     self.end_headers()
