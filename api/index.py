696850022b01b89958a049be604537afb8ee81b5
creuser <151720755+creuserr@users.noreply.github.com>
2024-02-15 23:11:18 +0800

    update index.py

--- api/index.py
+++ api/index.py
@@ -12,14 +12,14 @@ class handler(BaseHTTPRequestHandler):
       if path[0] == 'star':
         url += f'stargazers-{req["stargazers_count"]}-yellow'
       elif path[0] == 'watch':
-        url += f'watchers-{req["watchers_count"]}-violet'
+        url += f'watchers-{req["watchers_count"]}-#00A4FF'
       elif path[0] == 'sub':
-        url += f'subscribers-{req["subscribers_count"]}-peach'
+        url += f'subscribers-{req["subscribers_count"]}-DF6EFF'
       else:
-        url += f'400 Bad Request-Invalid method-lightred'
+        url += f'400 Bad Request-Invalid method-FF5353'
     except:
       if len(path) != 3:
-        url += f'400 Bad Request-#FF5353'
+        url += f'400 Bad Request-FF5353'
       else:
         url += f'500 Internal Error-FF2D2D'
     self.send_response(302)
