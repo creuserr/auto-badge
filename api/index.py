a683fc9b2ed4a7988ca5af7d0866cee6d2ae2e01
creuser <151720755+creuserr@users.noreply.github.com>
2024-02-15 23:16:54 +0800

    update index.py

--- api/index.py
+++ api/index.py
@@ -10,7 +10,7 @@ class handler(BaseHTTPRequestHandler):
       req = requests.get('https://creprox.vercel.app/https:/api.github.com/repos/' + '/'.join(path[1:]))
       req = json.loads(req.text)
       if 'Not Found' in req.text:
-        url += f'404-Not Found-FF5353'
+        url += f'404-{'/'.join(path[1:])} not found-FF5353'
       elif path[0] == 'star':
         url += f'stargazers-{req["stargazers_count"]}-D8BA00'
       elif path[0] == 'watch':
@@ -18,10 +18,10 @@ class handler(BaseHTTPRequestHandler):
       elif path[0] == 'sub':
         url += f'subscribers-{req["subscribers_count"]}-DF6EFF'
       else:
-        url += f'400 Bad Request-Invalid method-FF5353'
+        url += '400 Bad Request-Invalid method-FF5353'
     except:
       if len(path) != 3:
-        url += f'400 Bad Request-FF5353'
+        url += '400 Bad Request-FF5353'
       else:
         url += f'500 Internal Error-FF2D2D'
     self.send_response(302)
