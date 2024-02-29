e0b6d7d9153023e1f57f144038811de3f9c4e8fa
creuser <151720755+creuserr@users.noreply.github.com>
2024-02-29 11:57:31 +0800

    update listen.py

--- api/listen.py
+++ api/listen.py
@@ -5,3 +5,9 @@ import json
 
 class handler(BaseHTTPRequestHandler):
   def do_GET(self):
+    path = '/'.join(self.path[1:].split('/')[1:])
+    req = requests.get('https://lyrist.vercel.app/api/' + path)
+    res = json.loads(req.text)
+    title = res['title']
+    artist = res['artist']
+    image = res['image']
