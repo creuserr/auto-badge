b04f51587972c9332a0f1adadf819f54aacde9ec
creuser <151720755+creuserr@users.noreply.github.com>
2024-02-29 12:12:46 +0800

    update listen.py

--- api/listen.py
+++ api/listen.py
@@ -12,7 +12,7 @@ class handler(BaseHTTPRequestHandler):
     artist = res['artist']
     image = res['image']
     st = '{white-space:nowrap;overflow:hidden;text-overflow:ellipsis}'
-    svg = f'<svg xmlns="http://www.w3.org/2000/svg" width="400" height="100"><style>@import url("https://fonts.googleapis.com/css2?family=Inter");text{st}</style><defs><clipPath id="rounded-corner"><rect x="10" y="10" width="80" height="80" rx="3" ry="3"/></clipPath></defs><image href="{image}" x="10" y="10" width="80" height="80" clip-path="url(#rounded-corner)" /><text x="105" y="25" font-size="14" fill="lightgray" font-family="Inter">Listening to</text><text x="105" y="50" font-size="20" fill="black" font-family="Inter">{title}</text><text x="105" y="75" font-size="16" fill="gray" font-family="Inter">{artist}</text></svg>'
+    svg = f'<svg xmlns="http://www.w3.org/2000/svg" width="400" height="100"><style>@import url("https://fonts.googleapis.com/css2?family=Inter");.text{st}</style><defs><clipPath id="rounded-corner"><rect x="10" y="10" width="80" height="80" rx="3" ry="3"/></clipPath></defs><image href="{image}" x="10" y="10" width="80" height="80" clip-path="url(#rounded-corner)" /><text x="105" y="25" font-size="14" fill="lightgray" font-family="Inter" class="text">Listening to</text><text x="105" y="50" font-size="20" fill="black" font-family="Inter" class="text">{title}</text><text x="105" y="75" font-size="16" fill="gray" font-family="Inter" class="text">{artist}</text></svg>'
     self.send_response(200)
     self.send_header('Access-Control-Allow-Origin', '*')
     self.send_header('Content-Type', 'image/svg+xml')
